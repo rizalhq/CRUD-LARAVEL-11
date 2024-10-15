4db67bf8b7911a5bc66901f89fba7ab8085859a3
rizalhq <114711423+rizalhq@users.noreply.github.com>
2024-10-15 20:20:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ Route::middleware('guest')->group(function() {
 });
 
 Route::get('/siswa',[SiswaController::class, 'tampil'])->name('siswa.tampil');
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
 
 Route::middleware('auth')->group(function() {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/siswa/tambah',[SiswaController::class, 'tambah'])->name('siswa.tambah');
-    Route::post('siswa/submit',[SiswaController::class, 'submit'])->name('siswa.submit');
-    Route::get('siswa/edit{id}',[SiswaController::class, 'edit'])->name('siswa.edit');
-    Route::post('siswa/update{id}',[SiswaController::class, 'update'])->name('siswa.update');
-    Route::post('siswa/delete{id}',[SiswaController::class, 'delete'])->name('siswa.delete');
+    // Route::get('/siswa/tambah',[SiswaController::class, 'tambah'])->name('siswa.tambah');
+    // Route::post('siswa/submit',[SiswaController::class, 'submit'])->name('siswa.submit');
+    // Route::get('siswa/edit{id}',[SiswaController::class, 'edit'])->name('siswa.edit');
+    // Route::post('siswa/update{id}',[SiswaController::class, 'update'])->name('siswa.update');
+    // Route::post('siswa/delete{id}',[SiswaController::class, 'delete'])->name('siswa.delete');
+    
+    Route::post('/siswa/storeAjax', [SiswaController::class, 'storeAjax'])->name('siswa.storeAjax');
+    Route::get('/siswa/editAjax/{id}', [SiswaController::class, 'editAjax'])->name('siswa.editAjax');
+    Route::put('/siswa/updateAjax/{id}', [SiswaController::class, 'updateAjax'])->name('siswa.updateAjax');
+    Route::delete('/siswa/deleteAjax/{id}', [SiswaController::class, 'deleteAjax'])->name('siswa.deleteAjax');
 });
